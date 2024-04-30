fcca1f3cd1f6645335ff8c195aed47f1e2fb8db6
hyundee <hyundee9@gmail.com>
2024-04-30 17:24:02 +0900

    절대경로 설정 및 불필요한 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import "./App.css";
+import { ProfileCard } from "./components/ProfileCard";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <header>
+        Snap photos and share
+        <br /> like never before
+      </header>
+      <ProfileCard />
+    </div>
+  );
 }
 
 export default App;
